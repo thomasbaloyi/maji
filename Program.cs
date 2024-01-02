de9270ece60e2375e9465feb6d0a5dc8084fb15d
Thomas Baloyi <thomas.baloyi@pathcare.net>
2024-01-02 18:58:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// generate a map
+
+int mapSize = 20;
+Random randomNumberGenerator = new Random();
+
+int[,] terrain = new int[mapSize,mapSize];
+
+for (int outer = 0; outer < mapSize; outer++) { 
+    for (int inner = 0; inner < mapSize; inner++)
+    {
+        terrain[outer, inner] = randomNumberGenerator.Next(0, 4);
+    }
+}
+
+// start a gui
